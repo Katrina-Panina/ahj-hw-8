2ffcd838250bdcece35ebc9b6e14252484965af7
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-10-06 14:51:12 +0300

    fix

--- src/js/api/ChatAPI.js
+++ src/js/api/ChatAPI.js
@@ -1,11 +1,11 @@
-import Entity from "./Entity";
-import createRequest from "./createRequest";
+import Entity from './Entity';
+import createRequest from './createRequest';
 
 export default class ChatAPI extends Entity {
   create(data, callback) {
     const options = {
-      method: "POST",
-      query: "/newuser",
+      method: 'POST',
+      query: '/newuser',
       data,
       callback,
     };
